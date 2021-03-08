122b71a7cd3cb29692c5b05cbdbc6420de585b61
mizuki <75738412+mizuki51@users.noreply.github.com>
2021-03-09 00:03:37 +0900

    archive.php is completed

--- archive.php
+++ archive.php
@@ -1,72 +1,45 @@
-<?php get_header(); ?>    
-    
-    <main class="l-main">
-        <div class="l-main__left">
-            <div class="p-contents">
-                <div class="p-mainvisual--archive">
-                    <div class="p-mainvisual__text">
-                        <h2>Menu:</h2>
-                        <p>チーズバーガー</p>
-                    </div>
-                </div>
+<?php get_header(); ?>
 
-                <div class="p-box--archivepage">
-                    <h4>小見出しが入ります</h4>
-                    <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                </div>
-
-                <div class="p-menu-card">
-                    <img src="<?php echo esc_url ( get_template_directory_uri() . '/img/archive_card.png'); ?>" alt="">
-                    <div class="p-menu-card__intro">
-                        <h3>チーズバーガー</h3>
-                        <h4>小見出しが入ります</h4>
-                        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                        <a href="#">詳しく見る</a>
-                    </div>
-                </div>
-
-                <div class="p-menu-card">
-                    <img src="<?php echo esc_url ( get_template_directory_uri() . '/img/archive_card.png'); ?>" alt="">
-                    <div class="p-menu-card__intro">
-                        <h3>ダブルチーズバーガー</h3>
-                        <h4>小見出しが入ります</h4>
-                        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                        <a href="#">詳しく見る</a>
-                    </div>
-                </div>
-
-                <div class="p-menu-card">
-                    <img src="<?php echo esc_url ( get_template_directory_uri() . '/img/archive_card.png'); ?>" alt="">
-                    <div class="p-menu-card__intro">
-                        <h3>スペシャルチーズバーガー</h3>
-                        <h4>小見出しが入ります</h4>
-                        <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                        <a href="#">詳しく見る</a>
-                    </div>
-                </div>
-
-                <div class="p-pagenation--pc p-pagenation--tab">
-                    <ul>
-                        <li>page 1/10</li>
-                        <li><a href="/html/archive.html">1</a></li>
-                        <li><a href="/html/archive.html">2</a></li>
-                        <li><a href="/html/archive.html">3</a></li>
-                        <li><a href="/html/archive.html">4</a></li>
-                        <li><a href="/html/archive.html">5</a></li>
-                        <li><a href="/html/archive.html">6</a></li>
-                        <li><a href="/html/archive.html">7</a></li>
-                        <li><a href="/html/archive.html">8</a></li>
-                        <li><a href="/html/archive.html">9</a></li>
-                    </ul>
-                </div>
-                <div class="p-pagenation--sp">
-                    <a href="#">&lt;&lt; 前へ</a>
-                    <a href="#">次へ &gt;&gt;</a>
+<main class="l-main">
+    <div class="l-main__left">
+        <div class="p-contents">
+            <div class="p-mainvisual--archive">
+                <div class="p-mainvisual__text">
+                    <h2>Menu:</h2>
+                    <p><?php single_cat_title(); ?></p>
                 </div>
             </div>
+
+            <div class="p-box--archivepage">
+                <h4>小見出しが入ります</h4>
+                <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+            </div>
+
+            <?php
+            if (have_posts()) :
+                while (have_posts()) :
+                    the_post(); ?>
+                    <div class="p-menu-card" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <?php the_post_thumbnail(); ?>
+                        <div class="p-menu-card__intro">
+                            <h3><?php the_title(); ?></h3>
+                            <?php the_excerpt(); ?>
+                            <a href="<?php the_permalink(); ?>">詳しく見る</a>
+                        </div>
+                    </div>
+                <?php endwhile;
+            else :
+                ?><p>表示する記事がありません</p>
+            <?php endif; ?>
+
+
+            <div class="p-pagenation--pc p-pagenation--tab">
+                <?php if( function_exists("the_pagination") ) the_pagination(); ?>
+            </div>
         </div>
+    </div>
 
-        <?php get_sidebar(); ?>
-    </main>
+    <?php get_sidebar(); ?>
+</main>
 
-    <?php get_footer(); ?>
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
     add_theme_support( 'menus' );
     add_theme_support( 'title-tag' );
-    add_theme_support( 'post-thumbnails' ); //追加
+    add_theme_support( 'post-thumbnails' );
 
     function hamburger_title( $title ) {
         if ( is_front_page() && is_home() ) {
@@ -43,3 +43,44 @@
         );
     }
     add_action( 'widgets_init', 'hamburger_widgets_init' );
+    
+    function the_pagination() {
+        global $wp_query; //$wp_queryとはWordPressがページ読み込む時にデータベースから自動的に取得したさまざまなデータの集まりが格納されている変数
+                          //その変数にアクセスしたいのでglobalでグローバル宣言をする
+        $big = 999999999; //URLを変換するときに使う。ありえない数字にしておけばOK
+        if ( $wp_query->max_num_pages <= 1 ) //もし最大ページ数が1以下(ページネーションの必要がない)であれば、
+            return; //何もしないで。
+        echo '<nav class="pagination">'; //ページ数が2以上であれば<nav class="pagination">を出力して。
+        echo paginate_links( array( //中身はこんな感じで出力しといて
+            'base'         => str_replace( $big, '%#%', esc_url( get_pagenum_link($big) ) ),
+                //baseパラメータはページ番号付きのリンクを作るために使われるベースの URLを指定
+                //str_replace()はカッコ内で指定した文字列からURLを作る
+                //ecc_urlはURLの無害化を行う。セキュリティを固めるために書くべき
+                //get_pagenum_linkは与えられた数字($big)からページ番号のリンクを返す。https://xxxx.com/?paged=999999999/という出力になる
+                //%#%は999999999を%#%に置き換える指定
+                //以上を経て https://xxxx.com/?paged=%#% が設定される
+            'format'       => '',
+                //formatパラメータはページネーションの構造を指定するのに使う。''にしているのは初期値でOKですということ
+            'current'      => max( 1, get_query_var('paged') ),
+                //currentパラメータは現在のページを出す
+                //初期値は0だがこのままでは挙動がおかしくなるので指定する
+                //max関数で1以上を出すように指定
+                //get_query_varは指定したクエリデータを取得する
+                //pagedは現在のページを取得してくれる
+            'total'        => $wp_query->max_num_pages,
+                //totalパラメータは最大投稿数の取得をする
+                //$wp_queryのmax_num_pagesから取得するよーという指定
+            'prev_text'    => '&lt;&lt;',
+                //前へや<<など1つ前に戻るリンクをどんな文字列にしたいか
+            'next_text'    => '&gt;&gt;',
+                //次へや>>など1つ後ろへいくリンクをどんな文字列にしたいか
+            'type'         => 'list',
+                //どんな形で返して欲しいか。ここではHTMLのulタグを使ったリストを返してくれる
+            'end_size'     => 1,
+                //ページ番号のリストの両端（最初と最後）にいくつの数字を表示するか。今回は1つずつに指定
+            'mid_size'     => 1
+                //現在のページの両側にいくつの数字を表示するか。(現在のページは含まない)今回は1つずつに指定
+        ) );
+        echo '</nav>'; //中身出力終わったら閉じタグ出しといて
+    }
+      
